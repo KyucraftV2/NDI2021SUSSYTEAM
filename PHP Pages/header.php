344c3293d4b7325c3ebeba4d12c09811c285dd15
DrKureha_Heir <drkuheir@gmail.com>
2021-12-02 19:48:53 +0100

    Modifs pages php

--- PHP Pages/header.php
+++ PHP Pages/header.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <title>Accueil</title>
-</head>
-
 <body>
-
-
-
   <header>
     <nav>
       <a href="page_bateaux.html">Bateaux</a>
