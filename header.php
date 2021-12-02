8fc7b1d268485e1783ee86924db0ba95c90d0ed0
DrKureha_Heir <drkuheir@gmail.com>
2021-12-02 19:31:35 +0100

    Pages.php (Header et footer inclus

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>Accueil</title>
+</head>
+
+<body>
+
+
+
+  <header>
+    <nav>
+      <a href="page_bateaux.html">Bateaux</a>
+      <a href="page_naufrage.html">Naufragés</a>
+      <a href="page_sauveteur.html">Sauveteurs</a>
+    </nav>
+    <h1>Les Sauveteurs du Dunkerquois</h1>
+  </header>
---
+++ mainPage.php
@@ -0,0 +1,51 @@
+<?php
+include 'header.php'
+?>
+<main>
+
+  <p>
+    <!-- 1er paragraphe-->
+  </p>
+
+  <section>
+    <figure>
+      <!-- Image n°1 (positionné à gauche)-->
+    </figure>
+
+    <blockquote>
+      Les sauvetages ? Mais tout le monde en fait ...
+      Si un homme tombe dans la "Baille" est ce qu'on ,e va pas le chercher ?
+      Il n'y à cela aucun mérite...
+    </blockquote>
+  </section>
+  <section>
+    <figure>
+      <!-- Image n°2 (positionné à droite)-->
+      <figcaption>
+        <blockquote>
+          <!--Citation sous l'image n°2-->
+        </blockquote>
+      </figcaption>
+    </figure>
+  </section>
+  <section>
+    <span class="text_gras">
+      Texte en gras
+    </span>
+    <p>
+      Paragraphe sous le texte en gras
+    </p>
+  </section>
+  <section>
+    <h3>Coin Info</h3>
+    <p>
+      <!--Informations-->
+    </p>
+  </section>
+  <section>
+
+  </section>
+</main>
+<?php
+include 'footer.php'
+?>
