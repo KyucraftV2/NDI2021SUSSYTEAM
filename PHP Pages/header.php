1663570022728e94d712d91425081dfbffc9edf4
DrKureha_Heir <drkuheir@gmail.com>
2021-12-02 20:19:51 +0100

    Ajout dossier BDD

--- PHP Pages/header.php
+++ PHP Pages/header.php
@@ -4,6 +4,8 @@
       <a href="page_bateaux.html">Bateaux</a>
       <a href="page_naufrage.html">Naufragés</a>
       <a href="page_sauveteur.html">Sauveteurs</a>
+      <a href="formulaire_naufrage.html">Signaler un sauvetage</a>
+      <a href="formulaire.html">Ajouter un sauveteur</a>
     </nav>
     <h1>Les Sauveteurs du Dunkerquois</h1>
   </header>
